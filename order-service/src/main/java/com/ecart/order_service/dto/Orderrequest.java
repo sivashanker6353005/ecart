dc375faedf48070d595abfbbd7ceea42c5513724
sivashanker <sivashanker6353005@gmail.com>
2025-05-31 13:13:15 +0530

    this commit is regarding order-service need to complete the service,repository,controller

---
+++ order-service/src/main/java/com/ecart/order_service/dto/Orderrequest.java
@@ -0,0 +1,15 @@
+package com.ecart.order_service.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Orderrequest {
+
+	private List<orderLineItemsDto>orderLineItemsdto;
+}
